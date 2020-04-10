4e0d6cc84f5f1d7c980a0a5c63c6633748a184c3
Torrence Brown <torrenceb90@gmail.com>
2020-04-10 15:31:04 -0700

    Added conditional if no transactions added yet

---
+++ lib/models/item_model.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ItemModel {
+  final bool isChecked;
+  final String groceryItem;
+  final double amount;
+
+  ItemModel(this.isChecked, this.groceryItem, this.amount);
+}
--- lib/ui/shopping_list_view.dart
+++ lib/ui/shopping_list_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_list_project/models/item_model.dart';
 
 class ShoppingListView extends StatefulWidget {
   @override
@@ -6,26 +7,50 @@ class ShoppingListView extends StatefulWidget {
 }
 
 class _ShoppingListViewState extends State<ShoppingListView> {
+  ItemModel item;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Shopping list'),
       ),
-      body: ListView(
-        children: <Widget>[
-          Container(
-            child: Card(
-              child: ListTile(
-                leading: Icon(Icons.check_box),
-                title: Center(
-                  child: Text('Chicken Breast'),
+      body: item == null
+          ? Center(
+              child: Container(
+                child: Text(
+                  'No items added yet!',
+                  style: TextStyle(fontSize: 25.0),
                 ),
-                trailing: Text('\$ 6.59'),
               ),
+            )
+          : ListView(
+              children: <Widget>[
+                _buildItemContainer(),
+              ],
             ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          print('Add grocery item');
+        },
+      ),
+    );
+  }
+
+  Container _buildItemContainer() {
+    return Container(
+      padding: EdgeInsets.all(5.0),
+      child: Card(
+        child: ListTile(
+          leading: item.isChecked
+              ? Icon(Icons.check_box)
+              : Icon(Icons.check_box_outline_blank),
+          title: Center(
+            child: Text(item.groceryItem),
           ),
-        ],
+          trailing: Text(item.amount.toString()),
+        ),
       ),
     );
   }
